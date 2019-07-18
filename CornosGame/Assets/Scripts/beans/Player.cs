038aaf0efd26e02ba4ee4f423ba87e4101f06c0b
Alcides Cunha <alcides.cunha@ufrpe.br>
2019-07-17 22:52:42 -0300

    Mudanças básicas no script Player
    Adicionados getters e setters em formato de função explícita

--- CornosGame/Assets/Scripts/beans/Player.cs
+++ CornosGame/Assets/Scripts/beans/Player.cs
@@ -11,6 +11,66 @@ public class Player : MonoBehaviour
     private float sabPoints; //pontuação de Sabedoria, usada para magias de longo alcance (dano mágico)
     private float malPoints; //pontuação de Malícia, usada para resistir a dano mágico
 
+    public float getlifePoins()
+    {
+        return this.lifePoints;
+    }
+
+    public float getmanaPoins()
+    {
+        return this.manaPoints;
+    }
+
+    public float getatkPoins()
+    {
+        return this.atkPoints;
+    }
+
+    public float getdefPoins()
+    {
+        return this.defPoints;
+    }
+
+    public float getsabPoins()
+    {
+        return this.sabPoints;
+    }
+
+    public float getmalPoins()
+    {
+        return this.malPoints;
+    }
+
+    public void setlifePoins(float lifePoints)
+    {
+        this.lifePoints = lifePoints;
+    }
+
+    public void setmanaPoins(float manaPoints)
+    {
+        this.manaPoints = manaPoints;
+    }
+
+    public void setatkPoins(float atkPoints)
+    {
+        this.atkPoints = atkPoints;
+    }
+
+    public void setdefPoints(float defPoints)
+    {
+        this.defPoints = defPoints;
+    }
+
+    public void setsabPoints(float sabPoints)
+    {
+        this.sabPoints = sabPoints;
+    }
+
+    public void setmalPoints(float malPoints)
+    {
+        this.malPoints = malPoints;
+    }
+
     void Start()
     {
         

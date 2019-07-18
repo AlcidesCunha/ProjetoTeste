5949238e1b84dbf79bf4107ce5e35e40007b206c
Alcides Cunha <alcides.cunha@ufrpe.br>
2019-07-17 22:35:49 -0300

    Atualizações em scripts
    Adicionado script de movimento, colocado fundo de teste, criado script entidade responsável por guardar atributos do player

---
+++ CornosGame/Assets/Scripts/Move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rig;
+    private Vector2 moveController;
+    // Start is called before the first frame update
+
+    public void setSpeed(float velocidade)
+    {
+        speed = velocidade;
+    }
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movimento = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveController = movimento * speed;   
+    }
+
+    void FixedUpdate()
+    {
+        rig.MovePosition(rig.position + moveController * Time.fixedDeltaTime);
+    }
+}
---
+++ CornosGame/Assets/Scripts/beans/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private float lifePoints; //vida regen alterada com itens (função será necessária)
+    private float manaPoints; //mana regen alterada com itens (função será necessária)
+    private float atkPoints; //ataque utilizado por magias melee e ataques (dano físico)
+    private float defPoints; //defesa para dano físico
+    private float sabPoints; //pontuação de Sabedoria, usada para magias de longo alcance (dano mágico)
+    private float malPoints; //pontuação de Malícia, usada para resistir a dano mágico
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

48b585d09d7cfc64babd60f2e965b570e0b0201f
Alcides Cunha <alcides.cunha@ufrpe.br>
2019-07-28 19:19:36 -0300

    Skill do local implementado
    Versao inicial do mecanismo de skill setada ao clique

--- CornosGame/Assets/Scripts/GetMousePosition.cs
+++ CornosGame/Assets/Scripts/GetMousePosition.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class GetMousePosition : MonoBehaviour
 {
+
+
     public static Vector2 posicaoAtualDoMouse;
+
+    public GameObject bullet;
+
     void Start()
     {
         
@@ -13,9 +18,18 @@ public class GetMousePosition : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 mouseposition = -Vector3.one;
-        mouseposition = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0,0,5f));
-        Vector2 direcao = mouseposition;
-        posicaoAtualDoMouse = direcao;
+        //Vector3 mouseposition = -Vector3.one;
+        //mouseposition = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0,0,5f));
+        //Vector2 direcao = mouseposition;
+        //posicaoAtualDoMouse = direcao;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.LookAt(mousePosition);
+            Instantiate(bullet, transform.position, transform.rotation);
+            posicaoAtualDoMouse = new Vector2(mousePosition.x,mousePosition.y);
+        }
+
     }
 }
---
+++ CornosGame/Assets/Scripts/Projectile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+}
